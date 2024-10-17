ea279fc64f3859d7701f209ef04a4b0eb50143d1
Cauan Meira Guerreiro <160766478+Cauanmguerreiro@users.noreply.github.com>
2024-10-17 01:46:50 -0300

    Add files via upload

--- Jovemtech.py
+++ Jovemtech.py
@@ -23,3 +23,38 @@
 #faren = (celsius*1.8) + 32
 
 #print(faren)
+
+# Algoritimo de média
+
+# valor1 = 10
+# valor2 = 15
+# valor3 = 12
+
+# print(10+15+12/3)
+
+# #ou
+
+# valor1 =int(input("Insira o primeiro valor: "))
+# valor2 =int(input("Insira o segundo valor: "))
+# valor3 =int(input("Insira o terceiro valor: "))
+
+# print((valor1+valor2+valor3)/3)
+
+
+#Faça um programa que leia nome, idade e aniversário e imprima na tela
+
+# nome = str(input("Qual seu nome? "))
+# idade = int(input("Quantos anos você tem? "))
+
+
+# print("seu nome é" + nome + " sua idade é " + str(idade))
+
+
+#Faça um programa que receba como entrada a altura de uma pessoa e calcule o peso ideal usando a seguinte fórmula: (72.7*altura)-58
+
+# print("Vou verificar o peso ideal para você")
+
+# altura = float(input("Qual sua altura?"))
+# idealPeso = (72.7*altura)-58
+
+# print("o peso ideal para você é: " + str(idealPeso))
