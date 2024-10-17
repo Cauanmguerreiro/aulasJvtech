3b17ba1c4721457b5cb8f64f4edbe8eda457bc90
Cauanmguerreiro <cauanguerreiro@hotmail.com>
2024-10-17 01:06:38 -0300

    Primeiro commit

---
+++ Jovemtech.py
@@ -0,0 +1,25 @@
+#descubra se é par ou impar
+
+# valor = int(input("Insira o número que você quer saber se é par ou impar "))
+# if valor%2 == 0:
+#     print(" par")
+# else:
+#     print(" impar")
+
+
+
+#potencia 
+ 
+# a = 2**3
+# b = 5
+
+#print(a*b)
+
+
+#conversão temp
+
+#celsius = int(input("temp: "))
+
+#faren = (celsius*1.8) + 32
+
+#print(faren)
